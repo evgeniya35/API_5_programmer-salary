dabf9f25874e67f553e4f2a1cc296cb02fbc0608
evgeniya35 <evgeniya35@yandex.ru>
2021-11-11 16:54:02 +0300

    refactoring functions

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from dotenv import load_dotenv
 from terminaltables import SingleTable
 
 
-def lang_table(vacancies, title):
+def print_vacancies(vacancies_stat, title):
     rows = list()
     rows.append([
         "Язык программирования",
         "Вакансий найдено",
         "Вакансий обработано",
         "Средняя зарплата"])
-    for key, value in vacancies.items():
+    for lang, vacancies in vacancies_stat.items():
         rows.append([
-            key,
-            value["vacancies_found"],
-            value["vacancies_processed"],
-            value["average_salary"]])
+            lang,
+            vacancies["vacancies_found"],
+            vacancies["vacancies_processed"],
+            vacancies["average_salary"]])
     return SingleTable(rows, title).table
 
 
@@ -35,52 +35,56 @@ def predict_salary(salary_from, salary_to):
 
 
 def predict_rub_salary_sj(vacancy):
-    if vacancy["payment_from"] == 0 \
-        and vacancy["payment_to"] == 0 \
-            or vacancy["currency"] != "rub":
+    if not vacancy["payment_from"] and not vacancy["payment_to"] \
+        or vacancy["currency"] != "rub":
         return None
     return predict_salary(vacancy["payment_from"], vacancy["payment_to"])
 
 
 def predict_rub_salary_hh(vacancy):
-    if vacancy["salary"] is None or vacancy["salary"]["currency"] != "RUR":
+    if not vacancy["salary"] or vacancy["salary"]["currency"] != "RUR":
         return None
     return predict_salary(vacancy["salary"]["from"], vacancy["salary"]["to"])
 
 
-def fetch_vacancies_hh(language, area=1):
+def agregate_hh_vacancies(vacancies):
+    salaries = []
+    for vacancy in vacancies:
+        salary = predict_rub_salary_hh(vacancy)
+        if salary:
+            salaries.append(int(salary))
+    return salaries
+
+
+def find_hh_vacancies(language, area=1):
     payload = {
         "text": f"Программист {language}",
         "area": area,
         "per_page": 100
         }
-    salaries = []
     all_vacancies = []
     for page in count(0):
-        if payload["per_page"] * (page + 1) >= 2000: break
+        # if payload["per_page"] * (page + 1) >= 2000: break
         payload.update({"page": page})
         page_response = requests.get(
             "https://api.hh.ru/vacancies",
             params=payload
             )
         page_response.raise_for_status()
-        page_data = page_response.json()
-        all_vacancies.extend(page_data["items"])
-        if page >= page_data["pages"]:
+        vacancies = page_response.json()
+        all_vacancies.extend(vacancies["items"])
+        if page >= vacancies["pages"] - 10:
             break
-    vacancies_found = page_data["found"]
-    for vacancy in all_vacancies:
-        salary = predict_rub_salary_hh(vacancy)
-        if salary is not None:
-            salaries.append(int(salary))
-    return dict([
-        ("vacancies_found", vacancies_found),
-        ("vacancies_processed", len(salaries)),
-        ("average_salary", int(mean(salaries)))
-        ])
+    vacancies_found = vacancies["found"]
+    salaries = agregate_hh_vacancies(all_vacancies)
+    return {
+        "vacancies_found": vacancies_found,
+        "vacancies_processed": len(salaries),
+        "average_salary": int(mean(salaries))
+    }
 
 
-def fetch_vacancies_sj(language, sj_secret_key, town=4):
+def find_sj_vacancies(language, sj_secret_key, town=4):
     payload = {
         "keyword": f"Программист {language}",
         "town": town,
@@ -97,20 +101,20 @@ def fetch_vacancies_sj(language, sj_secret_key, town=4):
             headers=headers, params=payload
             )
         page_response.raise_for_status()
-        page_data = page_response.json()
-        all_vacancies.extend(page_data["objects"])
-        if not page_data["more"]:
+        vacancies = page_response.json()
+        all_vacancies.extend(vacancies["objects"])
+        if not vacancies["more"]:
             break
-    vacancies_found = page_data["total"]
+    vacancies_found = vacancies["total"]
     for vacancy in all_vacancies:
         salary = predict_rub_salary_sj(vacancy)
-        if salary is not None:
+        if salary:
             salaries.append(int(salary))
-    return dict([
-        ("vacancies_found", vacancies_found),
-        ("vacancies_processed", len(salaries)),
-        ("average_salary", int(mean(salaries)))
-        ])
+    return {
+        "vacancies_found": vacancies_found,
+        "vacancies_processed": len(salaries),
+        "average_salary": int(mean(salaries))
+    }
 
 
 def main():
@@ -124,10 +128,10 @@ def main():
     vacancies_summary_hh = dict()
     vacancies_summary_sj = dict()
     for language in languages:
-        vacancies_summary_hh[language] = fetch_vacancies_hh(language, 1)
-        vacancies_summary_sj[language] = fetch_vacancies_sj(language, sj_secret_key, 4)
-    print(lang_table(vacancies_summary_hh, "HeadHunter Moscow"))
-    print(lang_table(vacancies_summary_sj, "Supejob Moscow"))
+        vacancies_summary_hh[language] = find_hh_vacancies(language, 1)
+        vacancies_summary_sj[language] = find_sj_vacancies(language, sj_secret_key, 4)
+    print(print_vacancies(vacancies_summary_hh, "HeadHunter Moscow"))
+    print(print_vacancies(vacancies_summary_sj, "Supejob Moscow"))
 
 
 if __name__ == "__main__":
